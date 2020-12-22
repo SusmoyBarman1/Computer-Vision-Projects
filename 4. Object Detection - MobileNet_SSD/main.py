0d0dafca2d1013c254f9392aa96855faa0805d8c
SusmoyBarman1 <susmoy.barman.bd@gmail.com>
2020-12-22 15:54:31 +0600

    detection ongoing

--- 4. Object Detection - MobileNet_SSD/main.py
+++ 4. Object Detection - MobileNet_SSD/main.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 def getCOCOnames(classFile):
     with open(classFile, 'rt') as f:
@@ -20,17 +21,36 @@ def defineModel(architecturePath, weightsPath):
     return net
 
 
-def detectObjectFromImage(img, thress, model):
-    classId, confidence, boundingBox = model.detect(img, confThreshold=thress)
-    print(classId, boundingBox)
+def detectObjectFromImage(thress, model, cocoNames):
 
-    cv2.imshow('Output', img)
-    cv2.waitKey(0)
+    '''imgPath = 'data/tesla.jpg'
+    img = cv2.imread(imgPath)'''
+
+    videoPath = 'data/video.mp4'
+    vc = cv2.VideoCapture(videoPath)
+
+    while vc.isOpened():
+        ret, img = vc.read()
+        if not ret:
+            print("no frame:(")
+            break
+        
+        classIds, confidence, boundingBox = model.detect(img, confThreshold=thress)
+        #print(classIds.flatten())
+        #print(confidence.flatten())
+
+        for classId, conf, box in zip(classIds.flatten(), confidence.flatten(), boundingBox):
+            cv2.rectangle(img, box, color=(0, 255, 0), thickness=2)
+            cv2.putText(img, cocoNames[classId-1].upper(), (box[0]+10, box[1]+30), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+
+        cv2.imshow('Detection Output', img)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
 
 if __name__ == '__main__':
     # Paths
-    imgPath = 'data/office.jpg'
     classFile = 'Files/coco.names'
     architecturePath = 'Files/architecture_ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
     weightsPath = 'Files/weights_frozen_inference_graph.pb'
@@ -44,5 +64,4 @@ if __name__ == '__main__':
 
     model = defineModel(architecturePath, weightsPath)
 
-    img = cv2.imread(imgPath)
-    detectObjectFromImage(img, thres, model)
+    detectObjectFromImage(thres, model, cocoNames)
